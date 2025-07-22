9c51585718f3bbbaef58964acf07a6cb1d8de07c
Alba Lucia Gonzalez <alba.gonzalezibarguen@gmail.com>
2025-07-22 13:52:20 -0500

    Corrige import React frontend

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,4 +1,4 @@
-import React, { type PropsWithChildren } from 'react'
+import { type PropsWithChildren } from 'react'
 
 export const ErrorMessage = ({children} : PropsWithChildren) => {
   return (
